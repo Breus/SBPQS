a92e32edda86ac71110fe807033cbef69631301f
Blaauwendraad <breus.blaauwendraad@accenture.com>
2019-05-14 16:34:23 +0200

    Updated Lamport

--- Lamport/lamport.go
+++ Lamport/lamport.go
@@ -1,18 +1,39 @@
 package lamport
 
+import (
+	"crypto/rand"
+	"log"
+)
+
 /*
 * For this Lamport OTS implementation, we assume siging a 32-byte digest of the arbitrary length message
 * The wanted security level is 128 bit, thus for the random variables, we also pick 32-byte random variables
  */
 type KeyPair struct {
-	privateKey [32 * 32]byte
-	publicKey  [32 * 32]byte
+	privateKey privateKey
+	publicKey  publicKey
 }
 
-func KeyGen() (keypair KeyPair) {
-	for i := 0; i < len(message); i++ {
+type privateKey struct {
+	zero_vals [256][32]byte
+	one_vals  [256][32]byte
+}
 
+type publicKey struct {
+	h_zero_vals [256][32]byte
+	h_one_vals  [256][32]byte
+}
+
+func KeyGen() (keypair KeyPair, er error) {
+	for i := 0; i < 256; i++ {
+		_, err := rand.Read(keypair.privateKey.zero_vals[i][:])
+		if err != nil {
+			log.Fatal("Random reader crashed in key generation")
+			return
+		}
 	}
+
+	return
 
 }
 
